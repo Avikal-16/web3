3b5e9d05e027978811bb9e9f589149cf318037dc
Avikal Gupta <avikal.gupta2003@gmail.com>
2024-02-06 12:30:59 +0530

    edis for day2

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -18,15 +18,61 @@ contract Crowdfunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address_owner,string memory_title, string memory_description,
-    uint256_target, uint256_deadline, string memory_image) public returns (uint256) {}
-    
-    function donateToCampaign() {}
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function getDonators() {}
+        // is like if else or check kimnda statement in solidity
+        // deadline less than the current time and 
+        require(campaign.deadline < block.timestamp, " The deadline should be a date in the future. ");
 
-    function getCampaigns() {}
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
 
+        numberOfCampaigns++;   // after adding the data the number of campaigns is inceremented
 
+        return numberOfCampaigns-1;
+    }
 
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount= msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent){
+            campaign.amountCollected=campaign.amountCollected+amount;
+
+        }
+         
+
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return(campaigns[_id].donators, campaigns[_id].donations);
+
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        // creating a new array named allCampaigns  and assigning it to an 
+        // array of size which equals to the numberOfCampaigns.
+
+        for(uint i=0;i<numberOfCampaigns;i++){
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+
+        }
+
+        return allCampaigns;
+    }
 }
